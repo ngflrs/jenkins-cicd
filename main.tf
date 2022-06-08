998b1733518b0dd4205f44a2c177b2a0815804f7
angy <angycflo20@gmail.com>
2022-06-08 19:44:30 -0400

    Second commit to repo

--- main.tf
+++ main.tf
@@ -4,3 +4,54 @@ resource "aws_vpc" "main" {
     Name = "VPC-${var.tag}"
   }
 }
+
+# Create Public Subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.pub_subnet_cidr
+  availability_zone = var.av_zones.1
+  tags = {
+    Name = "Public-${var.tag}"
+  }
+}
+
+# Create Private Subnet
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.prv_subnet_cidr
+  availability_zone = var.av_zones.2
+  tags = {
+    Name = "Private-${var.tag}"
+  }
+}
+
+# Creating Internet Gateway
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "Internet-${var.tag}"
+  }
+}
+
+# # NAT Gateway
+# resource "aws_nat_gateway" "nat_gateway" {
+#   connectivity_type = "private"
+#   subnet_id         = aws_subnet.private_subnet.id
+#   tags = {
+#     Name = "NAT-${var.tag}"
+#   }
+# }
+
+# Creating Public Route Table
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+
+  tags = {
+    Name = "Public-${var.tag}-Table"
+  }
+}
